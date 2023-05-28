d378418d5a882f49cb38c79314d8cf7baa6e2b38
Tommy Putra <tommyputra734@gmail.com>
2023-05-28 20:47:42 +0700

    update the code

--- report/src/main/excel_report.py
+++ report/src/main/excel_report.py
@@ -1,3 +1,4 @@
+#%%
 from report.src.operators.xlsx_report_plugin import ExcelReportPlugin
 from report.src.utils.discord.discord_webhook import send_to_discord
 import os
@@ -9,15 +10,17 @@ print(f'base path: {base_path}')
 input_data = base_path + '/input_data/supermarket_sales.xlsx'
 output_data = base_path + '/output_data/daily_report_3.xlsx'
 
+
 # Opening JSON file
 configs = open(base_path + '/configs/webhook.json')
 webhook_url = json.load(configs)['webhook_url']
 
 automate = ExcelReportPlugin(
-    input_file=input_data,
-    output_file=output_data
+    input_data=input_data,
+    output_data=output_data
 )
 
 if __name__ == "__main__":
     automate.main()
     send_to_discord(webhook_url, output_data)
+# %%
